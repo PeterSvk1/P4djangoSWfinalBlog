25026aababbba8d3a3fcbcc6474974e07ff694a9
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-19 23:02:42 +0000

    deletepost works

--- ablog/urls.py
+++ ablog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import Home, Postdetail,NewPost, EditPost
+from .views import Home, Postdetail,NewPost, EditPost, DeletePost
 
 urlpatterns = [
     
     path('', Home.as_view(), name='home'),
     path('posts/<int:pk>', Postdetail.as_view(), name='details'),
     path('articles/', NewPost.as_view(), name='newpost'),
-     path('articles/edit/<int:pk>', EditPost.as_view(), name='editpost'),
+    path('articles/edit/<int:pk>', EditPost.as_view(), name='editpost'),
+    path('articles/delete/<int:pk>', DeletePost.as_view(), name='deletepost'),
   
 ]
