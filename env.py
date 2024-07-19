22bf5eb8d645f56b76718d40b504ce3f163008ea
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-19 19:44:29 +0000

    heroku deployement

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ["DATABASE_URL"] = "postgres://uluskwu6tad:e8ycZLkYfT3v@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mural_twins_mold_755211"
+os.environ["SECRET_KEY"] = "123456789"
+os.environ["CLOUDINARY_URL"] = "cloudinary://726681765859959:H2z2wDmqScGa2lYvXc18oNiijS8@dg5lyidc8"
--- starwars/settings.py
+++ starwars/settings.py
@@ -11,21 +11,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qyi0vsivbx9nkh5u=)967s@3!6+hg_b8tytd10-yzim_@gcpld'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-petersvk1-p4djangoswfin-jd9dk3bvca1.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-petersvk1-p4djangoswfin-jd9dk3bvca1.ws.codeinstitute-ide.net',".herokuapp.com"]
 
 
 # Application definition
@@ -36,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'ablog',
 ]
 
@@ -55,7 +64,7 @@ ROOT_URLCONF = 'starwars.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,12 +83,18 @@ WSGI_APPLICATION = 'starwars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
 
 
 # Password validation
@@ -119,6 +134,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
