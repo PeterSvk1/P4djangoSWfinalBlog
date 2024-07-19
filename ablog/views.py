bb8ac6eaf931c602a44bd428ba209ec07195e81d
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-19 22:38:08 +0000

    login/logout bootstrap

--- ablog/views.py
+++ ablog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import reverse_lazy,reverse
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 
 
@@ -22,4 +24,12 @@ class NewPost(CreateView):
     model = Post
     template_name = 'newpost.html'
     form_class = PostForm
-    success_url = reverse_lazy('home')
+   # success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            post= form.save(commit=False)
+            post.author= get_object_or_404(User, id=self.request.user.id)
+            post.save()
+            pk= post.id 
+            return HttpResponseRedirect(reverse('details', args=[str(pk)]))
