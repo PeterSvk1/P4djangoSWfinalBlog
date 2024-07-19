b35a7781a9d49f858ff96608da0d643fdafff5e7
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-19 22:45:15 +0000

    added url edit

--- ablog/urls.py
+++ ablog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('posts/<int:pk>', Postdetail.as_view(), name='details'),
     path('articles/', NewPost.as_view(), name='newpost'),
+     path('articles/edit/<int:pk>', EditPost.as_view(), name='editpost'),
   
 ]
