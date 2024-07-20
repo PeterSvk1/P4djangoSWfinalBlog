71b5f7fc0b382b82dad756b0be734d8a7a7600f1
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-20 19:38:59 +0000

    hide smtp

--- starwars/settings.py
+++ starwars/settings.py
@@ -79,8 +79,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'lineage4ever88@gmail.com'
-EMAIL_HOST_PASSWORD = 'sjlj hlcc lmdl naga'
+
 
 ROOT_URLCONF = 'starwars.urls'
 
