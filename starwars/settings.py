c728e87f2abd13c06b4c188f2b1f6f0ba09c1668
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-20 20:01:51 +0000

    mail is safe now

--- starwars/settings.py
+++ starwars/settings.py
@@ -79,7 +79,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 ROOT_URLCONF = 'starwars.urls'
 
