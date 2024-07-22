c1c63bf2960539983f8c8e5ea3bf83a68e699524
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-22 15:30:53 +0000

    styled login and registration html

--- starwars/settings.py
+++ starwars/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-petersvk1-p4djangoswfin-jd9dk3bvca1.ws.codeinstitute-ide.net',".herokuapp.com"]
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
+    'widget_tweaks',
     'ablog',
 ]
 
