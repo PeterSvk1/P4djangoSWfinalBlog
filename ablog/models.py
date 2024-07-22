334f7b4a1867691f66b0c83c130f595c6f662ffb
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-22 18:09:14 +0000

    fixed user registration

--- ablog/models.py
+++ ablog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime,date
 from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -83,3 +85,11 @@ class ContactMessage(models.Model):
     def __str__(self):
         return f"Message from {self.email} on {self.created_at}"
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
