ab4cba4b81e1c1cc449de7399b66d8be784a8e14
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-07-27 15:10:16 +0000

    herokudeploy

--- starwars/settings.py
+++ starwars/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-petersvk1-p4djangoswfin-jd9dk3bvca1.ws.codeinstitute-ide.net',".herokuapp.com"]
+ALLOWED_HOSTS = ['8000-petersvk1-p4djangoswfin-jd9dk3bvca1.ws.codeinstitute-ide.net',"projectstarwars.herokuapp.com"]
 
 # Application definition
 
